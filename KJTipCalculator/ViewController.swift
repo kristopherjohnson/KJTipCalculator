5fc27b19af02f35e0291b2dd7931d817705a974a
Kristopher Johnson <kris@kristopherjohnson.net>
2014-06-13 20:46:59 -0400

    Initial Commit

---
+++ KJTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KJTipCalculator
+//
+//  Created by Kristopher Johnson on 6/13/14.
+//  Copyright (c) 2014 Kristopher Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
