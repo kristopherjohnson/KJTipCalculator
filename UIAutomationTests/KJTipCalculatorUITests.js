442faa9f3c00c31b738017e71f779c283409f286
Kristopher Johnson <kris@kristopherjohnson.net>
2015-03-15 00:08:07 -0400

    Cleanup

--- UIAutomationTests/KJTipCalculatorUITests.js
+++ UIAutomationTests/KJTipCalculatorUITests.js
@@ -1,7 +1,5 @@
 // UI automation tests for KJTipCalculator
 
-UIALogger.logDebug("Start script")
-
 function assertEqual(value1, value2, description) {
     if (value1 !== value2) {
         throw new Error("Assertion failed: " + description + "; " + value1 + " != " + value2)
@@ -12,7 +10,7 @@ var target = UIATarget.localTarget()
 var app = target.frontMostApp()
 var window = app.mainWindow()
 
-if (true) window.logElementTree()
+// window.logElementTree()
 
 var checkSubtotal = window.textFields()["Check subtotal"]
 var tipPercentage = window.textFields()["Tip percentage"]
@@ -23,32 +21,36 @@ var total = window.staticTexts()["Total"]
 var perPerson = window.staticTexts()["Per person"]
 
 // Define test cases
+//
+// Each test case has a name and a test method
 var testCases = [{
+    name: "Test Clear",
+    test: function(name) {
+        checkSubtotal.setValue(999)
+        target.delay(0.5)
+
+        clearButton.tap()
+
+        // Need a short pause before text values are updated
+        target.delay(0.1)
+
+        assertEqual("Price", checkSubtotal.value(), "subtotal should be empty, so its value is placeholder text")
+        assertEqual("",      tip.value(),           "tip should be empty")
+        assertEqual("",      total.value(),         "total should be empty")
+        assertEqual("",      perPerson.value(),     "perPerson should be empty")
+
+        UIALogger.logPass(name)
+    }
+}, {
     name: "Basic test",
     test: function(name) {
         checkSubtotal.setValue(1000)
         tipPercentage.setValue(18)
         numberInParty.setValue(4)
 
-        assertEqual("180.00", tip.value(), "calculated tip")
-        assertEqual("1180.00", total.value(), "calculated total")
-        assertEqual("295.00", perPerson.value(), "calculated split")
-
-        UIALogger.logPass(name)
-    }
-}, {
-    name: "Test Clear button",
-    test: function(name) {
-        checkSubtotal.setValue(999)
-        clearButton.tap()
-
-        // Need a short pause before values are updated
-        target.delay(0.1)
-
-        assertEqual("Price", checkSubtotal.value(), "subtotal should be empty, so value is placeholder")
-        assertEqual("", tip.value(), "tip should be empty")
-        assertEqual("", total.value(), "total should be empty")
-        assertEqual("", perPerson.value(), "perPerson should be empty")
+        assertEqual( "180.00", tip.value(),       "calculated tip")
+        assertEqual("1180.00", total.value(),     "calculated total")
+        assertEqual( "295.00", perPerson.value(), "calculated split")
 
         UIALogger.logPass(name)
     }
