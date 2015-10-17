529fe00a6bccc1dee3d91525a08c00249ed1b1b8
Kristopher Johnson <kris@kristopherjohnson.net>
2015-10-17 12:46:27 -0400

    Add custom Calabash steps for checking output values

--- features/step_definitions/tip_calculator_steps.rb
+++ features/step_definitions/tip_calculator_steps.rb
@@ -8,3 +8,24 @@ When /^all input fields are cleared$/ do
   clear_text("textField marked: 'Tip percentage'")
   clear_text("textField marked: 'Number in party'")
 end
+
+Then /^tip should be "(.*)"$/ do |expected_text|
+  actual_text = query("label accessibilityLabel:'Tip'", :text).first
+  unless expected_text == actual_text
+    fail "Tip is incorrect. Expected '#{expected_text}', but got '#{actual_text}'."
+  end
+end
+
+Then /^total should be "(.*)"$/ do |expected_text|
+  actual_text = query("label accessibilityLabel:'Total'", :text).first
+  unless expected_text == actual_text
+    fail "Total is incorrect. Expected '#{expected_text}', but got '#{actual_text}'."
+  end
+end
+
+Then /^per-person should be "(.*)"$/ do |expected_text|
+  actual_text = query("label accessibilityLabel:'Per person'", :text).first
+  unless expected_text == actual_text
+    fail "Per-person is incorrect. Expected '#{expected_text}', but got '#{actual_text}'."
+  end
+end
